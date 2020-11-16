1abdc707f8bfc10c29223e083372cf5f2a853f05
Doog <63171864+doogwebdevgittutorial@users.noreply.github.com>
2020-11-16 17:59:16 -0600

    remove redundant <canvas> elt.

---
+++ threejs/lighting/main.js
@@ -0,0 +1,75 @@
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  DirectionalLight,
+} from '../libs/threejs/build/three.module.js';
+
+// get a reference to the container element that will hold our scene
+const container = document.querySelector('#scene-container');
+
+// create a Scene
+const scene = new Scene();
+
+// set the background color
+scene.background = new Color('skyblue');
+
+// create a camera
+const fov = 35; // field of view
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1; // the near clipping plane
+const far = 100; // the far clipping plane
+
+const camera = new PerspectiveCamera(fov, aspect, near, far);
+
+// every object is initally created at (0, 0, 0)
+// move the camera back so we can view the scene
+camera.position.set(0, 0, 10);
+
+// create a geometry
+const geometry = new BoxBufferGeometry(2, 2, 2);
+
+// create a default (white) Basic material
+const material = new MeshBasicMaterial();
+
+// create a Mesh containing the geometry and material
+const cube = new Mesh(geometry, material);
+
+// add the mesh to the scene
+scene.add(cube);
+
+// create the renderer, turn on antialiasing
+const renderer = new WebGLRenderer({ anialias: true});
+
+// turn on the physically correct lighting model
+renderer.physicallyCorrectLights = true;
+
+// next, set the renderer to the same size as our container element
+renderer.setSize(container.clientWidth, container.clientHeight);
+
+// finally, set the pixel ratio so that our scene will look good on HiDPI displays
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// add the automatically created <canvas> element to the page
+container.append(renderer.domElement);
+
+// render, or 'create a still image', of the scene
+renderer.render(scene, camera);
+
+
+
+// Handle responsiveness
+
+window.addEventListener("resize", function(){
+
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.render(scene, camera);
+})
